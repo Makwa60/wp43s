72a9b02a86540e311ea7ca3a1049941b184f9670
Benjamin Titmus <ben.titmus@cantab.net>
2022-10-14 20:48:44 +0100

    Change license header in mathematics files

--- src/wp43/mathematics/arg.c
+++ src/wp43/mathematics/arg.c
@@ -1,23 +1,5 @@
-/* This file is part of 43S.
- *
- * 43S is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * 43S is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with 43S.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/********************************************//**
- * \file arg.c
- ***********************************************/
-// Coded by JM, based on arctan.c
+// SPDX-License-Identifier: GPL-3.0-only
+// SPDX-FileCopyrightText: Copyright The WP43 Authors
 
 #include "mathematics/arg.h"
 
@@ -31,8 +13,6 @@
 #include "registers.h"
 
 #include "wp43.h"
-
-
 
 TO_QSPI void (* const arg[NUMBER_OF_DATA_TYPES_FOR_CALCULATIONS])(void) = {
 // regX ==> 1            2           3           4            5            6            7            8            9             10
--- src/wp43/mathematics/iteration.c
+++ src/wp43/mathematics/iteration.c
@@ -1,22 +1,5 @@
-/* This file is part of 43S.
- *
- * 43S is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * 43S is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with 43S.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/********************************************//**
- * \file iteration.c
- ***********************************************/
+// SPDX-License-Identifier: GPL-3.0-only
+// SPDX-FileCopyrightText: Copyright The WP43 Authors
 
 #include "mathematics/iteration.h"
 
@@ -28,8 +11,6 @@
 #include "registers.h"
 
 #include "wp43.h"
-
-
 
 #define ITER_ISE ((INC_FLAG << 2) | 0)
 #define ITER_ISG ((INC_FLAG << 2) | 1)
