12661bb04fa10df35a2f6de4aeaaa186257b699e
Benjamin Titmus <ben.titmus@cantab.net>
2022-11-01 09:41:34 +0000

    Minor changes in fnTone and fnBeep
    * Combined tone and static function to simplify
    * Assert that toneNum < 10 because TAM prevents a higher number so we
      shouldn't need to check

--- src/wp43/ui/tone.c
+++ src/wp43/ui/tone.c
@@ -6,32 +6,25 @@
 #include "flags.h"
 #include "hal/audio.h"
 #include "ui/screen.h"
+#include <assert.h>
 #include <stdio.h>
 
 #include "wp43.h"
 
 TO_QSPI uint32_t frequency[10] = {164814, 220000, 246942, 277183, 293665, 329628, 369995, 415305, 440000, 554365};
 
-static void _tonePlay(uint16_t toneNum) {
-  if(getSystemFlag(FLAG_QUIET)) {
-    return;
-  }
-  if(toneNum < 10) {
+void fnTone(uint16_t toneNum) {
+  assert(toneNum < 10);
+  if(!getSystemFlag(FLAG_QUIET)) {
     audioTone(frequency[toneNum]);
   }
 }
 
 
 
-void fnTone(uint16_t toneNum) {
-  _tonePlay(toneNum);
-}
-
-
-
 void fnBeep(uint16_t unusedButMandatoryParameter) {
-  _tonePlay(8);
-  _tonePlay(5);
-  _tonePlay(9);
-  _tonePlay(8);
+  fnTone(8);
+  fnTone(5);
+  fnTone(9);
+  fnTone(8);
 }
