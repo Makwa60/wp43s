c50d677c82263d1d8e2cd05c451ab00bfb970c86
MihailJP <mihailjp@gmail.com>
2023-01-31 19:34:55 +0900

    Revert "Enable FASTFN again for e^x" and "Use Kahan sum in real34Exp"
    This reverts commit 379a2be107488a2040b0f7ccf4b4192e0ca1aacc.
    This reverts commit d5b7695cc388f116ac4437764e8d975ee1ff9ce9.

--- src/wp43/mathematics/exp.c
+++ src/wp43/mathematics/exp.c
@@ -62,7 +62,7 @@ void fnExp(uint16_t unusedButMandatoryParameter) {
 
 // Abramowitz and Stegun §4.2.1
 void real34Exp(const real34_t *xin, real34_t *res) {
-  real34_t x, xx, k, r, d, rd;
+  real34_t x, xx, k, r, r0, r1;
   bool neg = real34IsNegative(xin);
   bool termOdd = true;
 
@@ -71,7 +71,8 @@ void real34Exp(const real34_t *xin, real34_t *res) {
   real34Copy(const34_1, &k);
   real34Copy(const34_1, &xx);
   real34Copy(const34_1, &xx);
-  real34Copy(const34_0, &d);
+  real34Copy(const34_1, &r0);
+  real34Copy(const34_0, &r1);
 
   // Taylor series
   do {
@@ -79,14 +80,12 @@ void real34Exp(const real34_t *xin, real34_t *res) {
 
     real34Multiply(&xx, &x, &xx);
     real34Divide(&xx, &k, &xx);
-    real34Subtract(&xx, &d, &xx); // <- Kahan compensation
-    real34Add(res, &xx, &rd);
-    real34Subtract(&rd, res, &d); // <- Kahan compensation
-    real34Subtract(&d, &xx, &d);  // <- Kahan compensation
-    real34Copy(&rd, res);
+    real34Add(res, &xx, res);
+    real34Add(termOdd ? &r1 : &r0, &xx, termOdd ? &r1 : &r0);
     real34Add(&k, const34_1, &k);
     termOdd = !termOdd;
   } while(!real34IsSpecial(res) && !real34CompareEqual(res, &r));
+  real34Add(&r0, &r1, res);
 
   if(neg) {
     real34Divide(const34_1, res, res);
