552dbd8a30c9fa07463110a8d2730de651393443
MihailJP <mihailjp@gmail.com>
2023-02-04 22:34:12 +0900

    Fix CNST

--- src/wp43/defines.h
+++ src/wp43/defines.h
@@ -344,7 +344,7 @@
   #define NUMBER_OF_DATA_TYPES_FOR_CALCULATIONS     10
 
   // Number of constants
-  #define NUMBER_OF_CONSTANTS_39                   202
+  #define NUMBER_OF_CONSTANTS_39                   203
   #define NUMBER_OF_CONSTANTS_51                    39
   #define NUMBER_OF_CONSTANTS_1071                   1
   #define NUMBER_OF_CONSTANTS_34                    44
