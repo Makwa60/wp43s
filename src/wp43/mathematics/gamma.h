ef296bf37f5013778a7219dc42cb97ce43aaf90d
MihailJP <mihailjp@gmail.com>
2023-02-25 11:31:04 +0900

    Complex lnΓ now selects better branch of the imaginary part of the result

--- src/wp43/mathematics/gamma.h
+++ src/wp43/mathematics/gamma.h
@@ -8,6 +8,7 @@
   #define GAMMA_H
 
   #include "defines.h"
+  #include "realType.h"
   #include <stdint.h>
 
   void fnGamma     (uint16_t unusedButMandatoryParameter);
@@ -28,5 +29,7 @@
   void lnGammaLonI (void);
   void lnGammaReal (void);
   void lnGammaCplx (void);
+
+  void complexLnGamma(const real_t *xReal, const real_t *xImag, real_t *rReal, real_t *rImag, realContext_t *realContext);
 
 #endif // !GAMMA_H
