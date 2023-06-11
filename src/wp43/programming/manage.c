141869a499e99fabba26631ca4d9b1bd6215a1b0
MihailJP <mihailjp@gmail.com>
2023-06-11 09:40:37 +0900

    Close alpha-related menus in PEM when finished

--- src/wp43/programming/manage.c
+++ src/wp43/programming/manage.c
@@ -624,7 +624,40 @@ static void _insertInProgram(const uint8_t *dat, uint16_t sizeInBytes) {
   dynamicMenuItem = _dynamicMenuItem;
 }
 
+static void _closeAlphaMenus(void) {
+  for(int i = 0; i < SOFTMENU_STACK_SIZE; ++i) {
+    switch(-softmenu[softmenuStack[0].softmenuId].menuItem) {
+      case MNU_ALPHAINTL:
+      case MNU_ALPHAintl:
+      case MNU_ALPHAMATH:
+      case MNU_ALPHA_OMEGA:
+      case MNU_alpha_omega:
+        popSoftmenu();
+        break;
 
+      case MNU_MyAlpha:
+        switch(-softmenu[softmenuStack[1].softmenuId].menuItem) {
+          case MNU_ALPHAINTL:
+          case MNU_ALPHAintl:
+          case MNU_ALPHAMATH:
+          case MNU_ALPHA_OMEGA:
+          case MNU_alpha_omega:
+            popSoftmenu();
+            break;
+          default:
+            softmenuStack[0].softmenuId = 0; // MyMenu
+            return;
+        }
+
+      default:
+        return;
+    }
+  }
+  // Just in case softmenuStack was filled with AIM-related menus
+  for(int i = 0; i < SOFTMENU_STACK_SIZE; ++i) {
+    softmenuStack[i].softmenuId = 0; // MyMenu
+  }
+}
 
 void pemAlpha(int16_t item) {
   #if !defined(TESTSUITE_BUILD)
@@ -672,9 +705,7 @@ void pemAlpha(int16_t item) {
         deleteStepsFromTo(currentStep, findNextStep(currentStep));
         clearSystemFlag(FLAG_ALPHA);
         calcModeUpdateGui();
-        if(softmenuStack[0].softmenuId == 1) { // MyAlpha
-          softmenuStack[0].softmenuId = 0; // MyMenu
-        }
+        _closeAlphaMenus();
         return;
       }
       else {
@@ -685,9 +716,7 @@ void pemAlpha(int16_t item) {
       pemCloseAlphaInput();
       --firstDisplayedLocalStepNumber;
       defineFirstDisplayedStep();
-      if(softmenuStack[0].softmenuId == 1) { // MyAlpha
-        softmenuStack[0].softmenuId = 0; // MyMenu
-      }
+      _closeAlphaMenus();
       return;
     }
 
@@ -716,9 +745,7 @@ void pemCloseAlphaInput(void) {
     currentStep = findNextStep(currentStep);
     ++firstDisplayedLocalStepNumber;
     firstDisplayedStep = findNextStep(firstDisplayedStep);
-    if(softmenuStack[0].softmenuId == 1) { // MyAlpha
-      softmenuStack[0].softmenuId = 0; // MyMenu
-    }
+    _closeAlphaMenus();
   #endif // !TESTSUITE_BUILD
 }
 
