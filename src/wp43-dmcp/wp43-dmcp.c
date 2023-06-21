84945b76dc65c6dd67cde3ffb607249fd38ca554
MihailJP <mihailjp@gmail.com>
2023-06-21 21:35:23 +0900

    Fix DMCP auto-off
    - Keep on while RUNTIM is on
    - Keep on while PGM is running not to interfere R/S or EXIT button
    
    The latter applies for infinite loops like:
    LBL 00
    INC X
    PAUSE 10
    GTO 00

--- src/wp43-dmcp/wp43-dmcp.c
+++ src/wp43-dmcp/wp43-dmcp.c
@@ -169,7 +169,7 @@ const char alpha_upper_transl[] = "_" // code 0 unused
 
 void dmcpResetAutoOff(void) {
   // Key is ready -> clear auto off timer
-  if(!key_empty() || (calcMode == cmTimerApp) || !getSystemFlag(FLAG_AUTOFF)) {
+  if(!key_empty() || (calcMode == cmTimerApp) || !getSystemFlag(FLAG_AUTOFF) || getSystemFlag(FLAG_RUNTIM) || programRunStop == PGM_RUNNING) {
     reset_auto_off();
   }
 }
