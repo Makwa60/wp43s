7a3978cdf496968faa84d852e1a4be0338147e6c
MihailJP <mihailjp@gmail.com>
2024-08-18 20:05:01 +0900

    Error 11 is now "RAM does not suffice"

--- src/wp43/error.c
+++ src/wp43/error.c
@@ -37,7 +37,7 @@ TO_QSPI const char *errorMessages[NUMBER_OF_ERROR_CODES] = {
 /*  8 */  "Out of range",
 /*  9 */  "Illegal digit in integer input for this base",
 /* 10 */  "Input is too long",
-/* 11 */  "RAM is full",
+/* 11 */  "RAM does not suffice",
 /* 12 */  "Stack clash",
 /* 13 */  "Operation is undefined in this mode",
 /* 14 */  "Word size is too small",
