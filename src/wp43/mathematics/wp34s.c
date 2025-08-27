657e90898cb4c023ea06c64d1240e52fb55cab46
Walter <walter.bonin@web.de>
2025-08-27 23:03:19 +0000

    Edit wp34s.c - minor cosmetic stuff for symmetry reasons

--- src/wp43/mathematics/wp34s.c
+++ src/wp43/mathematics/wp34s.c
@@ -30,7 +30,7 @@
 #include "wp43.h"
 
 /******************************************************
- * This functions are borrowed from the WP34S project
+ * These functions are borrowed from the WP34S project
  ******************************************************/
 
 // Have to be careful here to ensure that every function we call can handle
@@ -83,7 +83,8 @@ void WP34S_Cvt2RadSinCosTan(const real_t *an, angularMode_t angularMode, real_t 
 
     case amGrad: {
       WP34S_Mod(&angle, const_400,     &angle, realContext); // mod(angle, 400g) --> angle
-      break;
+      angularMode = amGrad;
+   break;
     }
 
     case amDegree:
