c67fd0d35085c9ed5d711fd96e66d4ab5f8f35b2
Walter <walter.bonin@web.de>
2025-08-27 22:26:12 +0000

    Edit wp34s.c - corrected the modulo reduction for angular mode = multiples of pi

--- src/wp43/mathematics/wp34s.c
+++ src/wp43/mathematics/wp34s.c
@@ -69,10 +69,15 @@ void WP34S_Cvt2RadSinCosTan(const real_t *an, angularMode_t angularMode, real_t 
   }
 
   switch(angularMode) {
-    case amRadian:
-    case amMultPi: {
+    case amRadian: {
       WP34S_Mod(&angle, const1071_2pi, &angle, realContext); // mod(angle, 2pi) --> angle
       angularMode = amRadian;
+      break;
+    }
+
+    case amMultPi: {
+      WP34S_Mod(&angle, const_2,       &angle, realContext); // mod(angle, 2mp) --> angle
+      angularMode = amMultPi;
       break;
     }
 
